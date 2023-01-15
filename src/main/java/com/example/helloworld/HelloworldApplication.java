513e1f7c8ebcaf2d2009591d16cfb897d13034bd
Ariel Dobkin <arieldob13@gmail.com>
2023-01-15 15:16:31 +0200

    first commit

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -3,11 +3,15 @@ package com.example.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author ariel ID:214776668
+ */
 @SpringBootApplication
 public class HelloworldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloworldApplication.class, args);
+        System.out.println("Hello world");
     }
 
 }
